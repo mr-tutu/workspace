6c8ca103e62fb4831af17cd2592935a7310dc592
mr-tutu <15651071778@163.com>
2021-07-03 09:49:20 +0800

    修改静态资源文件目录 Signed-off-by: mr-tutu <15651071778@163.com>

--- js/banner.js
+++ js/banner.js
@@ -1,12 +1,12 @@
 //定义图片的路径，存放到数组
-var url=['url(img/banner1.jpg)','url(img/banner2.jpg)','url(img/banner3.jpg)','url(img/banner4.jpg)'];
- //定义显示函数
-function pic(index){
+var url = ['url(../static/resume-images/banner1.jpg)', 'url(../static/resume-images/banner2.jpg)', 'url(../static/resume-images/banner3.jpg)', 'url(../static/resume-images/banner4.jpg)'];
+//定义显示函数
+function pic(index) {
    //通过循环和判断语句，如果传过来的参数和i对应，那么久吧数组的第i个值赋值给banner的背景图片属性。
-   for(var i=0;i<4;i++){
-    if(i==index){
-    document.getElementById('banner').style.backgroundImage=url[i];
-    //  console.log("xxxxxxxxxxxxxxxxxx")
-    }
+   for (var i = 0; i < 4; i++) {
+      if (i == index) {
+         document.getElementById('banner').style.backgroundImage = url[i];
+         //  console.log("xxxxxxxxxxxxxxxxxx")
+      }
    }
 }
